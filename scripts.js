e42afd6f4e2c91af71f926ac649bccb227801ea9
uwu-it <46907974+uwu-it@users.noreply.github.com>
2019-01-21 15:41:54 -0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,14 +1,6 @@
 <script>
-      function readURL(input) {
-            if (input.files && input.files[0]) {
-                var reader = new FileReader();
-
-                reader.onload = function (e) {
-                    $('#blah')
-                        .attr('src', e.target.result);
-                };
-
-                reader.readAsDataURL(input.files[0]);
-            }
-        }
+var loadFile = function(event) {
+	var image = document.getElementById('output');
+	image.src = URL.createObjectURL(event.target.files[0]);
+};
 </script>
